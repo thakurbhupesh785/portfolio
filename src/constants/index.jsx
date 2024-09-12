0ccc5ce30e2afeb5de83eff855c6176f318058cb
Bhupesh-Thakur <153527966+thakurbhupesh785@users.noreply.github.com>
2024-09-12 11:20:33 -0700

    Update index.jsx

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -26,7 +26,7 @@ export const PROJECTS = [
     description:
       "A personal portfolio website built with React and Tailwind CSS to showcase my skills, projects, and contact information.",
     image: projectImage1,
-    link: "",
+    link: "https://bhupeshkumar07.netlify.app",
   },
   {
     id: 2,
